6132581b454acb4c64693c0a27b658eff8c7c659
IlzeG <71811509+IlzeG@users.noreply.github.com>
2021-01-06 10:00:35 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-console.log('Script started');
+console.log('FizzBuzz script started');
 
 function innit () {
     console.log('Adding listeners');
@@ -10,22 +10,6 @@ function innit () {
     var submitCustom = document.getElementById('custom');
     submitCustom.addEventListener("click", onClickCustom);
 }
-
-var slider1 = document.getElementById("elWidth");
-var slider1Value = document.getElementById("elWidthOutput");
-slider1.innerHTML = slider1Value.value;
-
-slider1.oninput = function() {
-  slider1Value.innerHTML = this.value;
-};
-
-var slider2 = document.getElementById("elHeight");
-var slider2Value = document.getElementById("elHeightOutput");
-slider2.innerHTML = slider2Value.value;
-
-slider2.oninput = function() {
-  slider2Value.innerHTML = this.value;
-};
 
 function onClick() {
     onClickClear();
@@ -84,26 +68,17 @@ function onClickCustom (){
     console.log('buzz color - ' + buzzColor);
     console.log('fizzbuzz color - ' + fizzBuzzColor);
     console.log('none color - ' + noneColor);
+
         for (startValue; startValue<=endValue; startValue++){
             const element = document.createElement('div');
             element.id="element" + startValue;
             
-            elements.appendChild(element);
-                if (startValue % fizzValue === 0 && startValue % buzzValue === 0){
-                    element.setAttribute("id", "element" +startValue);
-                    element.setAttribute("class", "FizzBuzz");
-                    element.innerText="FizzBuzz " + startValue;
-                    element.style.backgroundColor=fizzBuzzColor;
-                    element.style.width=slider1.value + "px";
-                    element.style.height=slider2.value + "px";
-                }
-                else if (startValue % fizzValue === 0 && startValue % buzzValue !== 0) {
+            elements.appendChild(element); 
+                if (startValue % fizzValue === 0 && startValue % buzzValue !== 0) {
                     element.setAttribute("id", "element" +startValue);
                     element.setAttribute("class", "Fizz");
                     element.innerText="Fizz " + startValue;
                     element.style.backgroundColor=fizzColor;
-                    element.style.width=slider1.value + "px";
-                    element.style.height=slider2.value + "px";
                 }
 
                 else if (startValue % buzzValue === 0 && startValue % fizzValue !== 0){
@@ -111,18 +86,19 @@ function onClickCustom (){
                     element.setAttribute("class", "Buzz");
                     element.innerText="Buzz " + startValue;
                     element.style.backgroundColor=buzzColor;
-                    element.style.width=slider1.value + "px";
-                    element.style.height=slider2.value + "px";
                 }
 
+                else if (startValue % fizzValue === 0 && startValue % buzzValue === 0){
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "FizzBuzz");
+                    element.innerText="FizzBuzz " + startValue;
+                    element.style.backgroundColor=fizzBuzzColor;
+                }
                 else if (startValue % fizzValue !== 0 && startValue % buzzValue !== 0){
                     element.setAttribute("id", "element" +startValue);
                     element.setAttribute("class", "Empty");
                     element.innerText=startValue;
                     element.style.backgroundColor=noneColor;
-                    element.style.width=slider1.value + "px";
-                    element.style.height=slider2.value + "px";
-
                 }
         }
 }
@@ -131,7 +107,5 @@ function onClickCustom (){
     const submit = document.getElementById('default');
     submit.onclick = onClick;
 }
-
-
 
 innit();
